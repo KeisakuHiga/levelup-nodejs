7a3c69e75ecddd2c00d606131e5a2c49b7046f41
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2020-10-09 22:40:06 +0900

    create a worker automatically when a worker is down unexpectedly

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const cluster = require('cluster');
+cluster.schedulingPolicy = cluster.SCHED_RR; // road balancer setting
 const http = require('http');
 const numCPUs = require('os').cpus().length;
+
 
 if (cluster.isMaster) {
   console.log(`master ${process.pid} is running`);
@@ -13,11 +15,16 @@ if (cluster.isMaster) {
   // catch the end of worker
   cluster.on('exit', (worker, code, signal) => {
     console.log(`worker ${worker.process.pid} died`);
+    console.log(`exit ${code} : ${signal}`);
+    // create a worker when an unexpected end occur
+    if (code !== 0 && !worker.exitedAfterDisconnect) {
+      cluster.fork();
+    }
   });
 } else {
   http.createServer((req, res) => {
     res.writeHead(200);
-    res.end('hello world\n');
+    res.end(`hello world ${process.pid} \n`);
   }).listen(8000);
 
   console.log(`worker ${process.pid} started`);
